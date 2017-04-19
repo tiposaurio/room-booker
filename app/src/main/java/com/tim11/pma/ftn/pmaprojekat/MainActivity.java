845c92fe7f0ff88a785fc24395658212d5535ef1
jansulja <shuky11@rocketmail.com>
2017-04-19 12:11:02 +0200

    Navigation drawer menu with icons

--- app/src/main/java/com/tim11/pma/ftn/pmaprojekat/MainActivity.java
+++ app/src/main/java/com/tim11/pma/ftn/pmaprojekat/MainActivity.java
@@ -80,17 +80,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_hotel_list) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_filter) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
